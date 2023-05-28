86dcba2082a3454b794ba24fa1e6797842c8b177
macarronesc <alegandro2507@gmail.com>
2023-05-28 21:27:59 +0000

    Refactoriced knative-mem MXFaaS done
    TO-DO:
    Parallel download (lithops? MiniO?)

--- cloudlab/knative-mem.py
+++ cloudlab/knative-mem.py
@@ -1,9 +1,10 @@
 import subprocess
-import time
 import numpy as np
 import threading
 import requests
 from statistics import mean, median,variance,stdev
+import re
+import time
 
 # get the url of a function
 def getUrlByFuncName(funcName):
@@ -18,70 +19,65 @@ def getUrlByFuncName(funcName):
             url = line.split()[1]
             return url
 
-serviceName = "mem-bandwidth"
-service = getUrlByFuncName(serviceName)
-
 def lambda_func(service):
     global times
     t1 = time.time()
     r = requests.post(service, json={"name": "test"})
     t2 = time.time()
-    times.append(t2-t1)
 
-def EnforceActivityWindow(start_time, end_time, instance_events):
-    events_iit = []
-    events_abs = [0] + instance_events
-    event_times = [sum(events_abs[:i]) for i in range(1, len(events_abs) + 1)]
-    event_times = [e for e in event_times if (e > start_time)and(e < end_time)]
-    try:
-        events_iit = [event_times[0]] + [event_times[i]-event_times[i-1]
-                                         for i in range(1, len(event_times))]
-    except:
-        pass
-    return events_iit
+    # Convert bytes literal to string
+    result_str = r.content.decode("utf-8")
 
+    # Use regular expression to extract the number
+    result_match = re.search(r'"result = ": ([0-9.e+-]+)', result_str)
+
+    if result_match:
+        result = float(result_match.group(1))
+        print(result)
+        times.append(t2-t1 - result)
+    else:
+        times.append(t2-t1)
+
+
+serviceName = "mem-bandwidth"
+service = getUrlByFuncName(serviceName)
 loads = [5]
-
 output_file = open("run-all-out.txt", "w")
 
-indR = 0
+print("Service: " + str(service))
+
 for load in loads:
     print("LOAD: " + str(load))
-    duration = 1
-    seed = 100
-    rate = load
-    # generate Poisson's distribution of events 
-    inter_arrivals = []
-    np.random.seed(seed)
-    beta = 1.0/rate
-    oversampling_factor = 2
-    inter_arrivals = list(np.random.exponential(scale=beta, size=int(oversampling_factor*duration*rate)))
-    instance_events = EnforceActivityWindow(0,duration,inter_arrivals)
-        
-    print("Service: " + str(service))
+
+    # MXFaaS
     threads = []
     times = []
-    after_time, before_time = 0, 0
 
-    st = 0
-    for t in instance_events:
-        print("Instance event: " + str(t))
-        st = st + t - (after_time - before_time)
-        before_time = time.time()
-        if st > 0:
-            time.sleep(st)
-
+    for i in range(5, 0, -1):
         threadToAdd = threading.Thread(target=lambda_func, args=(service, ))
         threads.append(threadToAdd)
         threadToAdd.start()
-        after_time = time.time()
 
     for thread in threads:
         thread.join()
 
+    print("MXFaaS", file=output_file, flush=True)
     print("=====================" + serviceName + "=====================", file=output_file, flush=True)
     print(mean(times), file=output_file, flush=True)
     print(median(times), file=output_file, flush=True)
     print(np.percentile(times, 90), file=output_file, flush=True)
     print(np.percentile(times, 95), file=output_file, flush=True)
     print(np.percentile(times, 99), file=output_file, flush=True)
+
+
+
+    # Parallel
+    times = []
+
+
+
+
+
+    print("", file=output_file, flush=True)
+    print("Parallel download", file=output_file, flush=True)
+    print("=====================" + serviceName + "=====================", file=output_file, flush=True)
--- cloudlab/mem_bandwidth/app.py
+++ cloudlab/mem_bandwidth/app.py
@@ -3,16 +3,19 @@ import os
 import time
 
 def lambda_handler():
+    start_time = time.time()
+    
     myfile = "/tmp/azurefunctions-accesses-2020.csv.bz2"
     # If file exists, delete it.
     if os.path.isfile(myfile):
         os.remove(myfile)
 
-    start_time = time.time()
-    gluon.utils.download('https://azurecloudpublicdataset2.blob.core.windows.net/azurepublicdatasetv2/azurefunctions_dataset2020/azurefunctions-accesses-2020.csv.bz2',path='/tmp/')
     end_time = time.time()
 
-    time = 'Time taken to execute the line: %f seconds' % (end_time - start_time)
-    print(time)
+    # Subtract the time to delete the file (this is not the fault of the original architecture)
+    elapsed_time = end_time - start_time
+    print(elapsed_time)
 
-    return {"result = ":time}
+    gluon.utils.download('https://azurecloudpublicdataset2.blob.core.windows.net/azurepublicdatasetv2/azurefunctions_dataset2020/azurefunctions-accesses-2020.csv.bz2',path='/tmp/')
+
+    return {"result = ":elapsed_time}
