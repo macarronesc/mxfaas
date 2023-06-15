a4fd8d87ed1d760af9b1e0ce533be0bf0584a4c4
macarronesc <alegandro2507@gmail.com>
2023-06-15 10:47:40 +0000

    Improved performIO method (faster) Implemented a waiting algorithm (faster)
    TO-DO:
    Improve the time measurements and make more similar to the original
    
    Compare with MXFaaS and original

--- KNative_prototype/knative-all.py
+++ KNative_prototype/knative-all.py
@@ -53,7 +53,7 @@ def EnforceActivityWindow(start_time, end_time, instance_events):
         pass
     return events_iit
 
-loads = [1, 5, 10]
+loads = [1, 5, 10, 15, 20]
 
 output_file = open("run-all-out.txt", "w")
 
